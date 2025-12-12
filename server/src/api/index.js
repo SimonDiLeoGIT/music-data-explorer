3f384755ca6d24c6353860ef8c73f625ca8bdb76
simon <saimondiley@gmail.com>
2025-12-12 18:57:47 +0000

    change vercel.json

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,4 @@
 import app from "../src/app.js";
 import serverless from "serverless-http";
 
-export const GET = serverless(app);
-export const POST = serverless(app);
-export const PUT = serverless(app);
-export const DELETE = serverless(app);
+export default serverless(app);
